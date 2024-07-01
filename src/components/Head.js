7dfd48858c41d73649c3fc015f724669fdd2719f
ekatakumari <ekatakumari207@gmail.com>
2024-07-01 17:27:25 +0530

    add

--- src/components/Head.js
+++ src/components/Head.js
@@ -39,47 +39,6 @@ const Head = () => {
     dispatch(toggleMenu());
   };
   return (
-    //     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
-    //       <div className="flex col-span-1 ">
-    //         <img
-    //           className="h-8 cursor-pointer"
-    //           alt="icon"
-    //           src={HAMBERGER_ICON}
-    //           onClick={toggleMenuHandler}
-    //         />
-    //         <a href="/">
-    //           <img className="h-8 mx-2" alt="logo" src={YOUTUBE_LOGO} />
-    //         </a>
-    //       </div>
-    //       <div className="col-span-10 px-10 flex flex-col">
-    //   <div className="flex">
-    //     <input
-    //       className="w-1/2 border border-gray-400 rounded-l-full p-2"
-    //       type="text"
-    //       value={searchQuery}
-    //       onChange={(e) => setSearchQuery(e.target.value)}
-    //     />
-    //     <button className="border border-gray-400 p-2 rounded-r-full bg-gray-100">
-    //       <img src={SEARCH_ICON} alt="search_icon" className="h-7" />
-    //     </button>
-    //   </div>
-    //   {searchQuery && (
-    //     <div className="w-[26rem] border border-gray-400 rounded-b-md bg-white mt-12 shadow-lg fixed py-2 px-5">
-    //       <ul>
-    //         <li className="p-2 border-b border-gray-300 hover:bg-gray-100">Iphone</li>
-    //         <li className="p-2 border-b border-gray-300 hover:bg-gray-100">Iphone</li>
-    //         <li className="p-2 border-b border-gray-300 hover:bg-gray-100">Iphone</li>
-    //         <li className="p-2 border-b border-gray-300 hover:bg-gray-100">Iphone</li>
-    //         <li className="p-2 hover:bg-gray-100">Iphone</li>
-    //       </ul>
-    //     </div>
-    //   )}
-    // </div>
-
-    //       <div className="col-span-1">
-    //         <img className="h-8" alt="user_icon" src={USER_ICON} />
-    //       </div>
-    //     </div>
     <div className="grid grid-flow-col p-5 m-2 shadow-lg items-center">
       {/* Left section with hamburger icon and logo */}
       <div className="flex col-span-1 items-center">
